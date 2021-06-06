706600fdd3b26cdefc4fd9f5ece410a5fafc46ad
Ryan Harding <darkstar100x@MacBook-Pro.local>
2021-06-06 13:23:59 -0400

    Initial commit

---
+++ src/main/java/base/PasswordValidator.java
@@ -0,0 +1,28 @@
+package base;
+
+import java.util.Scanner;
+
+public class PasswordValidator {
+  Scanner input = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    PasswordValidator pass = new PasswordValidator();
+    String pw = pass.obtainPassword();
+    System.out.println(pass.checkPassword(pw));
+  }
+
+  public String obtainPassword() {
+    System.out.print("What is the password? ");
+    return input.next();
+  }
+
+  public String checkPassword(String pw) {
+    String truePassword = "abc$123";
+    if (pw.equals(truePassword)) {
+      return "Welcome!";
+    }
+    else {
+      return "I don't know you.";
+    }
+  }
+}
